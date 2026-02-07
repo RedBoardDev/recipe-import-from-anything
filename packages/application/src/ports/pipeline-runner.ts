34a50834b05b05766cad4787c21e98ec61f8ef4a
RedBoardDev <thomas.ott@epitech.eu>
2026-02-07 17:29:37 +0200

    chore(api): remove deprecated API files and configurations, including routes, schemas, and tests

--- packages/application/src/ports/pipeline-runner.ts
+++
@@ -1,5 +0,0 @@
-import type { ImportJob, ImportResult } from "@ria/domain";
-
-export interface PipelineRunner {
-  run(job: ImportJob): Promise<ImportResult>;
-}
--- packages/application/src/workflow/index.ts
+++
@@ -1,3 +0,0 @@
-export type { Step, StepContext, StepRunOutput, WorkflowDefinition, WorkflowRunner } from "./types";
-export { LinearWorkflowRunner } from "./linear-workflow-runner";
-export { StepExecutionError } from "./step-error";
