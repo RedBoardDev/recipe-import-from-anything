6b8fe7012ee5c6206d96ad770303eeeb04c65bf8
RedBoardDev <thomas.ott@epitech.eu>
2026-02-07 17:40:12 +0200

    chore(infrastructure): remove obsolete infrastructure package files including configuration, migrations, and repository implementations

--- packages/infrastructure/src/queue/index.ts
+++
@@ -1,4 +0,0 @@
-export { BullMqJobQueue } from "./bullmq-job-queue";
-export { BullMqWorkerRuntime } from "./bullmq-worker-runtime";
-export { getRedisConnection } from "./redis";
-export type { RedisConnectionOptions } from "./redis";
